4bca8a4de4dd83847970e425f2685224320ebac2
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-04-17 11:53:39 +0200

    Initial commit, create Data Project

--- ForumOwl/Startup.cs
+++ ForumOwl/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ForumOwl.Data;
-using ForumOwl.Models;
 using ForumOwl.Services;
+using ForumOwl.Data.Models;
 
 namespace ForumOwl
 {
