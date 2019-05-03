37099083750f8731d7d259ba8ccbbf6eedcc985f
gergelyEsztojka <esztojka.gergely@gmail.com>
2019-05-03 12:05:48 +0200

    Commit for CV project start GitHub repo

---
+++ ForumOwl/Models/Forum/ForumTopicModel.cs
@@ -0,0 +1,11 @@
+﻿using ForumOwl.Models.Post;
+using System.Collections.Generic;
+
+namespace ForumOwl.Models.Forum
+{
+    public class ForumTopicModel
+    {
+        public ForumListingModel Forum { get; set; }
+        public IEnumerable<PostListingModel> Posts { get; set; }
+    }
+}
